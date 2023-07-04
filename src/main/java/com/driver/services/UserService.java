ead185c88af18b158b7200336b956f7e420c2052
Areeff <p.areeff786@gmail.com>
2023-07-04 11:02:45 +0530

    User implementation done

--- src/main/java/com/driver/services/UserService.java
+++ src/main/java/com/driver/services/UserService.java
@@ -25,16 +25,36 @@ public class UserService {
     public Integer addUser(User user){
 
         //Jut simply add the user to the Db and return the userId returned by the repository
-        return null;
+        User user1=userRepository.save(user);
+        return user1.getId();
     }
 
     public Integer getAvailableCountOfWebSeriesViewable(Integer userId){
 
         //Return the count of all webSeries that a user can watch based on his ageLimit and subscriptionType
         //Hint: Take out all the Webseries from the WebRepository
-
-
-        return null;
+        List<WebSeries>webSeriesList=webSeriesRepository.findAll();
+        User user=userRepository.findById(userId).get();
+        Subscription subscription=user.getSubscription();
+        Integer count=0;
+        for(WebSeries webSeries:webSeriesList){
+            if(user.getAge()>webSeries.getAgeLimit()){
+                if(subscription.getSubscriptionType()==SubscriptionType.ELITE){
+                    count++;
+                }
+                else if(subscription.getSubscriptionType()==SubscriptionType.PRO){
+                    if(webSeries.getSubscriptionType()==SubscriptionType.PRO||webSeries.getSubscriptionType()==SubscriptionType.BASIC){
+                        count++;
+                    }
+                }
+                else{
+                    if(webSeries.getSubscriptionType()==SubscriptionType.BASIC){
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
     }
 
 
